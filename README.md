871f4c51d0b6866bfb7ca918935d4312152ba77e
Ivan G <aloneguid@outlook.com>
2020-11-16 17:39:22 +0000

    intro into unit testing :)

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # bigninja
 
+[![PyPI](https://img.shields.io/pypi/v/bigninja)](https://pypi.org/project/bigninja/) ![PyPI - License](https://img.shields.io/pypi/l/bigninja)
+
 ```python
 from bigninja import *
 ```
 
 ## DataFrame
+
+### .wc_select(pattern: str)
+
+### .display()
+
+### .union_anyway(df: DataFrame)
 
--- bigninja/dataframe.py
+++ bigninja/dataframe.py
@@ -1,4 +1,6 @@
-from pyspark.sql import DataFrame
+from typing import Dict, List, Any
+
+from pyspark.sql import DataFrame, Row
 import re
 
 from pyspark.sql.types import *
@@ -19,5 +21,49 @@ def display_with_json(self: DataFrame) -> None:
     self.show(truncate=False)
 
 
+def union_anyway(self: DataFrame, df2: DataFrame) -> DataFrame:
+    # take first df and add missing columns, filling them with nulls
+    df1u = self
+    for df2c in df2.columns:
+        if df2c not in self.columns:
+            df1u = (df1u.withColumn(df2c, f.lit(None)))
+
+    # take df2 and make it idential in order and columns to df1u
+    cols2 = []
+    for df1c in df1u.columns:
+        if df1c in df2.columns:
+            # add own column
+            cols2.append(f.col(df1c))
+        else:
+            # add a dummy
+            cols2.append(f.lit(None).alias(df1c))
+    df2u = df2.select(*cols2)
+    df = df1u.union(df2u)
+
+    return df
+
+
+def as_list_of_dict(self: DataFrame) -> List[Dict[str, Any]]:
+    row_list: List[Row] = self.collect()
+    return [r.asDict(True) for r in row_list]
+
+
+def head_as_dict(self: DataFrame) -> Dict[str, Any]:
+    r: Row = self.head()
+    return r.asDict(True)
+
+
+def col_as_list(self: DataFrame, col_name: str = None) -> List[Any]:
+    df = self
+    if col_name:
+        df = df.select(col_name)
+    rows = df.collect()
+    return [x[0] for x in rows]
+
+
 DataFrame.wc_select = wc_select
 DataFrame.display = display_with_json
+DataFrame.union_anyway = union_anyway
+DataFrame.as_list_of_dict = as_list_of_dict
+DataFrame.head_as_dict = head_as_dict
+DataFrame.col_as_list = col_as_list
